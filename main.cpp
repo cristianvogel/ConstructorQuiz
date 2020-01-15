6b134c5f6314138c667029b59b3ab88dd89f2a36
Cristian Vogel <cristian.vogel55@gmail.com>
2020-01-15 16:21:15 +0000

    solution 2

--- main.cpp
+++ main.cpp
@@ -1,30 +1,20 @@
+/*
+
+this is what happens with no default constructor ( an implicit constructor is invoked)
+
+and no default member variable values ....
+
+big ball!
+
+*/
+
 #include <iostream>
 
-class Ball 
+struct Ball 
 {
-private:
-    std::string m_colour = "black";
-    int m_radius = 10.0;
-
-public:
-   // Ball() = default;
-
-    Ball( std::string colour = "black", int radius = 10.0) 
-    {
-        m_colour = colour;
-        m_radius = radius;
-    }
-/*
-    Ball(std::string colour) 
-    {
-        m_colour = colour;
-    }
-*/
-    Ball(int radius) 
-    {
-        m_radius = radius;
-    }
-
+    std::string m_colour;
+    int m_radius;
+   
     void print() 
     {
         std::cout << "Colour: " << m_colour << "\t radius: " << m_radius << "\n";
@@ -36,15 +26,6 @@ int main()
 {
 	Ball def;
 	def.print();
- 
-	Ball blue("blue");
-	blue.print();
-	
-	Ball twenty(20.0);
-	twenty.print();
-	
-	Ball blueTwenty("blue", 20.0);
-	blueTwenty.print();
- 
+
 	return 0;
 }
